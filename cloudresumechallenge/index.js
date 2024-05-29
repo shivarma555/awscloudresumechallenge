1600c4aa71f0628e659ab65efd980569b0b79c8a
Shivam Sharma <shivam.sharma3@corp.renewbuy.com>
2024-05-29 15:42:32 +0530

    second commit

--- cloudresumechallenge/index.js
+++ cloudresumechallenge/index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter(){
     let response = await fetch("https://bpsjjex7syfu2fex4wbzndzvu40sheit.lambda-url.eu-north-1.on.aws/");
     let data = await response.json();
-    Counter.innerHTML = ` Views: ${data}`;
+    counter.innerHTML = ` Views: ${data}`;
 }
 
 updateCounter()
